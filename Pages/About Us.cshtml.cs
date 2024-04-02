d168f980d9e663c53529548d8656bbd44d1afeab
Zaeem <128582944+Zaeem78@users.noreply.github.com>
2024-04-02 04:45:49 +0200

    Add files via upload

---
+++ Pages/About Us.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _10316317POE_Part1.Pages
+{
+    public class About_UsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Contact Us.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _10316317POE_Part1.Pages
+{
+    public class Contact_UsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
